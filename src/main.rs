18a1dd93a6cfebc939804cae1b9aa348bb6a81d7
mike schmitt <michael.schmitt@maine.edu>
2022-11-29 12:07:09 -0500

    found wav data, will send via serial to DAC?

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,14 @@
 mod sd_commands;
 mod pwm;
 use rppal::spi::{Spi, Bus, Mode, SlaveSelect};
+use rppal::uart::{Uart, Parity};
+//use crate::pwm::pwm_init::{pwm_init};
 use crate::sd_commands::sd_init::{sd_init};
+use crate::sd_commands::sd_write::*;
 use crate::sd_commands::sd_read::{one_block_pretty_print,
                                   read_sd_1_block,
                                   sd_multiblock_read, 
                                   multiblock_pretty_print};
-use crate::sd_commands::sd_write::*;
-use crate::pwm::pwm_init::{pwm_init};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> 
 {
@@ -32,21 +33,19 @@ fn main() -> Result<(), Box<dyn std::error::Error>>
     }
 
     // data start (fat table)
-    let mut sector: u32 = 0x800 + 32 + 0x3b80*2;
-    println!("\nsector: {:x}", sector);
-    sd_send_cmd(spi, CMD_17, sector)?;
-    one_block_pretty_print(read_sd_1_block(spi)?);
-
-    // data for test.txt start (4 sectors long)
-    sector = 0x820 + 0x3b80*2 + (0x000d-0x2)*32;
+    let mut sector: u32 = 0x800 + 32 + 0x3b80*2; //2*FATSz sectors past reserved
     println!("\nsector: {:x}", sector);
     sd_send_cmd(spi, CMD_17, sector)?;
     one_block_pretty_print(read_sd_1_block(spi)?);
 
     println!("\npretty:\n");
-    multiblock_pretty_print(sd_multiblock_read(spi, 0x8080, 3)?);
+    let marley_sector: u32 = sector + (0x0e-2)*32;
+    multiblock_pretty_print(sd_multiblock_read(spi, marley_sector+20, 10)?);
 
-    pwm_init().unwrap();
+    let mut uart = Uart::new(115_200, Parity::None, 8, 1).unwrap();
+    sd_send_cmd(spi, CMD_17, sector)?;
+    let data = read_sd_1_block(spi)?.1;
+    uart.write(&data)?;
     
     return Ok(());
 }
