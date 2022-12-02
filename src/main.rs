2d3564d23cdb3f8ac7264b0f37aa4f0a10897c5c
mike schmitt <michael.schmitt@maine.edu>
2022-12-02 16:55:20 -0500

    working (?) dac output with triangle wave

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@
 
 mod sd_commands;
 mod pwm;
+use std::{thread, time};
 use rppal::spi::{Spi, Bus, Mode, SlaveSelect};
 use rppal::uart::{Uart, Parity};
 //use crate::pwm::pwm_init::{pwm_init};
@@ -33,19 +34,56 @@ fn main() -> Result<(), Box<dyn std::error::Error>>
     }
 
     // data start (fat table)
-    let mut sector: u32 = 0x800 + 32 + 0x3b80*2; //2*FATSz sectors past reserved
+    let sector: u32 = 0x800 + 32 + 0x3b80*2; //2*FATSz sectors past reserved
     println!("\nsector: {:x}", sector);
     sd_send_cmd(spi, CMD_17, sector)?;
     one_block_pretty_print(read_sd_1_block(spi)?);
 
     println!("\npretty:\n");
+    // wav data start
     let marley_sector: u32 = sector + (0x0e-2)*32;
-    multiblock_pretty_print(sd_multiblock_read(spi, marley_sector+20, 10)?);
+    let bird_sector: u32 = sector + (0x0571-2)*32;
+    sd_send_cmd(spi, CMD_17, marley_sector)?;
+    one_block_pretty_print(read_sd_1_block(spi)?);
+
+    multiblock_pretty_print(sd_multiblock_read(spi, marley_sector+25, 10)?);
+    multiblock_pretty_print(sd_multiblock_read(spi, bird_sector, 10)?);
 
     let mut uart = Uart::new(115_200, Parity::None, 8, 1).unwrap();
-    sd_send_cmd(spi, CMD_17, sector)?;
-    let data = read_sd_1_block(spi)?.1;
-    uart.write(&data)?;
+    let mut some: u32 = 0;
+    let mut buf_to_dac: [u8; 512] = [0x00; 512];
+    let read_len: usize = 2;
+    let mut sample: i16;
+    const SAMPLE_SIZE: usize = 2;
+    let mut sample_to_dac: [u8; SAMPLE_SIZE] = [0xff; SAMPLE_SIZE];
+
+    loop {
+        for i in (0..256).step_by(SAMPLE_SIZE) {
+            for j in 0..SAMPLE_SIZE {
+                sample_to_dac[j] = (j + i) as u8;
+            }
+            uart.write(&sample_to_dac);
+
+            // (samples / sample rate) - 1
+            thread::sleep(time::Duration::from_micros(44));
+        }
+    }
     
-    return Ok(());
+    //loop {
+    //    let data = sd_multiblock_read(spi, marley_sector+some, read_len)?.4;
+    //    for i in 0..2 {
+    //        for j in 0..256 {
+    //            println!("{:02x}, {:02x}", data[i][j*2], data[i][j*2+1]);
+    //            sample = ((data[i][j*2+1] as i16) <<8)+((data[i][j*2]) as i16);
+    //            println!("{:04x}", sample);
+    //            sample_to_dac = (((sample as i32) + (1<<15)) >> 8) as u8;
+    //            println!("{:02x}", sample_to_dac);
+    //            buf_to_dac[j + i*j] = sample_to_dac;
+    //        }
+    //    }
+    //    uart.write(&buf_to_dac)?;
+    //    some += 2;
+    //}
+    
+    //return Ok(());
 }
